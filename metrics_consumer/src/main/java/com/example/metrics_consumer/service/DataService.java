6bca1aa370deb6fef16a4d3e921170bdf74a1161
boris86msk <boris86msk@yandex.ru>
2024-04-24 22:42:22 +0300

    fix DataService.java

--- metrics_consumer/src/main/java/com/example/metrics_consumer/service/DataService.java
+++ metrics_consumer/src/main/java/com/example/metrics_consumer/service/DataService.java
@@ -1,6 +1,6 @@
 package com.example.metrics_consumer.service;
 
-import ru.openshcool.metrics_consumer.dto.MetricDto;
+import com.example.metrics_consumer.dto.MetricDto;
 
 public interface DataService {
     void saveMetric(MetricDto metric, String topicName);
